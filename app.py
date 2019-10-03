aa233b84705b7a1e15df35d8826ec903493ef834
Christopher Barnes <cbarnes7404@yahoo.com>
2019-10-03 13:14:00 -0700

    add nav bar adjust display

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor_Shopp
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 items = db.items
-#comments = db.comments
+comments = db.comments
 
 app = Flask(__name__)
 
@@ -17,12 +17,35 @@ app = Flask(__name__)
 def items_index():
     """Return homepage."""
     """Show all items."""
-    return render_template('index.html')
-    #return render_template('index.html', items=items.find())
+    #return render_template('index.html')
+    return render_template('index.html', items=items.find())
 
 # CREATE NEW
 @app.route('/item/new')
 def items_new():
     """Create a new item."""
-    return render_template('new_item.html')
-#    return render_template('new_item.html', item={}, title='New Item')
+#    return render_template('new_item.html')
+    return render_template('new_item.html', item={}, title='New Item')
+
+# DISPLAY/SHOW
+@app.route('/items', methods=['POST'])
+def items_submit():
+    """Submit a new item."""
+    item = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        #'videos': request.form.get('videos').split(),
+        'created_at': datetime.now(),
+        'ratings': request.form.get('ratings')
+    }
+    print(item)
+    item_id = items.insert_one(item).inserted_id
+    return redirect(url_for('show_items', item_id=item_id))
+
+# DISPLAY/SHOW FROM ID
+@app.route('/items/<item_id>')
+def show_items(item_id):
+    """Show a single item."""
+    item = items.find_one({'_id': ObjectId(item_id)})
+    item_comments = comments.find({'item_id': ObjectId(item_id)})
+    return render_template('show_item.html', item=item, comments=item_comments)
